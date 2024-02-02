5c94a7c31dcf7cc6ec633d08fb820af3cdddf941
Marco87Developer <marco87developer@gmail.com>
2024-02-02 16:58:12 -0500

    Initial project

---
+++ lib/moneys.dart
@@ -0,0 +1,7 @@
+/// Manage money, currencies, budgets and other related things with ease.
+///
+library;
+
+// export 'src/moneys_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
